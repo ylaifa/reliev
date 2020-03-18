969edd5a1fcfaacfc087c8a5e59b4925a106f8d0
OnyJ <julien.josselin75@hotmail.fr>
2020-03-18 19:14:31 +0100

    edit routes to get about and contact pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get 'about',   to: 'static_pages#about'
+  get 'contact', to: 'static_pages#contact'
 end
