297c5061df026e9a142cbdac5214a5aeac85ab6e
Yousri <laifayousri@gmail.com>
2020-03-19 14:36:18 +0100

    edit seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+
+Company.destroy_all
+
+5.times do
+    Company.create(email: Faker::Internet.email, password: "azerty")
+end
