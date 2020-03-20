64b3c7e62c7d0dde82440c33eb83caa1e5d3ba6b
OnyJ <julien.josselin75@hotmail.fr>
2020-03-20 14:49:53 +0100

    edit development.rb - remove lines of local testing

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -62,7 +62,5 @@ Rails.application.configure do
   # Devise 
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
 
-  # Letter opener (locally opens emails)
-  config.action_mailer.delivery_method = :letter_opener
   config.action_mailer.perform_deliveries = true
 end
