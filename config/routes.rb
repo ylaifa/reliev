eefb8547ea0590c13f0470bda355cc94cf4d1c14
Yousri <laifayousri@gmail.com>
2020-03-20 22:45:30 +0100

    add resources to routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :masseurs
   devise_for :companies
   devise_for :employees
-  resources  :employee_profiles, only: [:show, :edit, :update]
+  resources  :company_profiles, only: [:show, :edit, :update]
   
   root 'welcome#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
