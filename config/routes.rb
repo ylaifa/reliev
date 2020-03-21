b42c6b6f38d4d342e6cb8e967f95973b8022ec58
OnyJ <julien.josselin75@hotmail.fr>
2020-03-21 03:04:00 +0100

    edit routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,11 +3,12 @@ Rails.application.routes.draw do
   devise_for :masseurs
   devise_for :companies, controllers: { registrations: "registrations" }
 
-  resources  :company_profiles, only: [:show, :edit, :update]
+  resources  :masseur_profiles,  only: [:show, :edit, :update]
+  resources  :company_profiles,  only: [:show, :edit, :update]
   resources  :employee_profiles, only: [:show, :edit, :update]
   
   root 'welcome#index'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get 'about',   to: 'static_pages#about'
   get 'contact', to: 'static_pages#contact'
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
