5c7b8e169ac383cd94a136f854369b25bd0d0a46
OnyJ <julien.josselin75@hotmail.fr>
2020-03-21 01:41:26 +0100

    create masseur profile migration

---
+++ db/migrate/20200321003031_create_masseur_profiles.rb
@@ -0,0 +1,13 @@
+class CreateMasseurProfiles < ActiveRecord::Migration[5.2]
+  def change
+    create_table :masseur_profiles do |t|
+      t.string :first_name
+      t.string :last_name
+      t.decimal :pricing
+      t.text :description
+      t.belongs_to :company, index: true
+
+      t.timestamps
+    end
+  end
+end
