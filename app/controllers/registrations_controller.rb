9a4088b08dc3f12f63243fb3aa8630848b2cd884
Yousri LAIFA <32313527+Yous91@users.noreply.github.com>
2020-03-21 15:19:47 +0100

    Edit registrations_controller.rb

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -5,7 +5,7 @@ class RegistrationsController < Devise::RegistrationsController
     if @employee
       edit_employee_profile_path(@employee)
     elsif @masseur
-      edit_masseur_profile_path(@masseur)
+      edit_masseur_profile_path(@masseur.masseur_profile)
     elsif @company
       edit_company_profile_path(@company.company_profile)
     end
