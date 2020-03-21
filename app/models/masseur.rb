8e851a9cfadcf0d7c336a332dc8e2df008d483bc
OnyJ <julien.josselin75@hotmail.fr>
2020-03-21 12:27:15 +0100

    edit masseur model - add profile relationships

--- app/models/masseur.rb
+++ app/models/masseur.rb
@@ -1,10 +1,19 @@
 class Masseur < ApplicationRecord
+  after_create :create_profile
+  after_create :welcome_send
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  after_create :welcome_send
+  has_one :masseur_profile, dependent: :destroy
+
+private
+
+  def create_profile
+    MasseurProfile.create(masseur: self)
+  end
 
   def welcome_send
     UserMailer.welcome_email(self).deliver_now
