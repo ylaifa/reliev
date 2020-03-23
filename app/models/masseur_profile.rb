9ce9dc30a52a11789da9fd8ec2268799b004c557
OnyJ <julien.josselin75@hotmail.fr>
2020-03-23 14:49:53 +0100

    edit masseur_profile model - add validates to fields

--- app/models/masseur_profile.rb
+++ app/models/masseur_profile.rb
@@ -1,3 +1,8 @@
 class MasseurProfile < ApplicationRecord
+  validates :first_name, presence: true, :on => :update
+  validates :last_name, presence: true, :on => :update
+  validates :price, presence: true, :on => :update
+  validates :description, presence: true, :on => :update
+
   belongs_to :masseur
 end
