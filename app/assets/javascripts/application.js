bcf102bb07c342eb13d4e2db0dfbd8cbe4c97947
OnyJ <julien.josselin75@hotmail.fr>
2020-03-23 20:20:21 +0100

    add calling template libraries in manifest

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,4 +13,26 @@
 //= require rails-ujs
 //= require activestorage
 //= require turbolinks
-//= require_tree .
+//= bootstrap.min.js
+//= hs.core.js
+//= hs.fancybox.js
+//= hs.focus-state.js
+//= hs.go-to.js
+//= hs.header.js
+//= hs.malihu-scrollbar.js
+//= hs.megamenu.js
+//= hs.show-animation.js
+//= hs.slick-carousel.js
+//= hs.svg-injector.js
+//= hs.unfold.js
+//= hs.validation.js
+//= jquery.fancybox.min.js
+//= jquery.mCustomScrollbar.concat.min.js
+//= jquery-migrate.min.js
+//= jquery.min.js
+//= jquery.validate.min.js
+//= popper.min.js
+//= slick.js
+//= svg-injector.min.js
+//= typed.min.js
+    
