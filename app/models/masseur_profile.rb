8cfc383e26a655c1b847d26da0bd271696497339
Yousri <laifayousri@gmail.com>
2020-03-24 17:16:24 +0100

    edit masseur_profile model

--- app/models/masseur_profile.rb
+++ app/models/masseur_profile.rb
@@ -1,10 +1,13 @@
 class MasseurProfile < ApplicationRecord
+  belongs_to :masseur
+
   validates :first_name, presence: true, :on => :update
   validates :last_name, presence: true, :on => :update
-  validates :price, presence: true, :on => :update
+  validates :pricing, presence: true, :on => :update
   validates :description, presence: true, :on => :update
-
-  belongs_to :masseur
+  validates :massage_types, presence: true, :on => :update
+  validates :time_slots, presence: true, :on => :update
+  validates :working_place, presence: true, :on => :update
 
   MASSAGE_TYPES = ["Suédois", "Californien", "Bulgare", "Thaïlandais", "Japonais", "Express", "Tonique", "Plantaire", "Relaxant"].freeze
   TIME_SLOTS = ["Lundi matin", "Lundi après-midi", "Mardi matin", "Mardi après-midi", "Mercredi matin", "Mercredi après-midi", "Jeudi matin", "Jeudi après-midi", "Vendredi matin", "Vendredi après-midi"].freeze
