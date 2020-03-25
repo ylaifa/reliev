914f98ae9b733f99993f2dbfaec4fea6a276f638
Raphael Chappert <raphael.chappert@hotmail.fr>
2020-03-25 23:01:04 +0100

    rails generate devise_invitable Employee

--- app/models/employee.rb
+++ app/models/employee.rb
@@ -4,7 +4,7 @@ class Employee < ApplicationRecord
  
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable,
+  devise :invitable, :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
   has_one :employee_profile, dependent: :destroy
