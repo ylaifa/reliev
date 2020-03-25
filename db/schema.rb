d9d8f35f9c9188856bf813f2e640e8f6990eb79f
Raphael Chappert <raphael.chappert@hotmail.fr>
2020-03-25 23:07:06 +0100

    run rails db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,23 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_23_210602) do
+ActiveRecord::Schema.define(version: 2020_03_25_215832) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
-
-  create_table "addresses", force: :cascade do |t|
-    t.string "street_number"
-    t.string "route"
-    t.string "locality"
-    t.string "administrative_area_level_1"
-    t.string "postal_code"
-    t.string "country"
-    t.bigint "company_profile_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["company_profile_id"], name: "index_addresses_on_company_profile_id"
-  end
 
   create_table "companies", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -65,7 +52,19 @@ ActiveRecord::Schema.define(version: 2020_03_23_210602) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "invitation_token"
+    t.datetime "invitation_created_at"
+    t.datetime "invitation_sent_at"
+    t.datetime "invitation_accepted_at"
+    t.integer "invitation_limit"
+    t.string "invited_by_type"
+    t.bigint "invited_by_id"
+    t.integer "invitations_count", default: 0
     t.index ["email"], name: "index_employees_on_email", unique: true
+    t.index ["invitation_token"], name: "index_employees_on_invitation_token", unique: true
+    t.index ["invitations_count"], name: "index_employees_on_invitations_count"
+    t.index ["invited_by_id"], name: "index_employees_on_invited_by_id"
+    t.index ["invited_by_type", "invited_by_id"], name: "index_employees_on_invited_by_type_and_invited_by_id"
     t.index ["reset_password_token"], name: "index_employees_on_reset_password_token", unique: true
   end
 
