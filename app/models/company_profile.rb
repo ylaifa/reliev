40ea7ac496a3594e69433d42cb25e71145be75a4
Raphael Chappert <raphael.chappert@hotmail.fr>
2020-03-26 11:21:20 +0100

    add associations invitations to company_profile model

--- app/models/company_profile.rb
+++ app/models/company_profile.rb
@@ -2,4 +2,5 @@ class CompanyProfile < ApplicationRecord
     validates :name, presence: true, :on => :update
     
     belongs_to :company
+    has_many :invitations, class_name: self.to_s, as: :invited_by
 end
