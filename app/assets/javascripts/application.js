d834478b569a0c70161b758830b630558ac94ba6
OnyJ <julien.josselin75@hotmail.fr>
2020-03-26 11:15:22 +0100

    edit - ordered require

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,27 +12,32 @@
 //
 //= require rails-ujs
 //= require activestorage
+//= require turbolinks
 //= require jquery3
 //= require jquery_ujs
-//= require bootstrap.min.js
-//= require hs.core.js
-//= require hs.fancybox.js
-//= require hs.focus-state.js
-//= require hs.go-to.js
-//= require hs.header.js
-//= require hs.malihu-scrollbar.js
-//= require hs.megamenu.js
-//= require hs.show-animation.js
-//= require hs.slick-carousel.js
-//= require hs.svg-injector.js
-//= require hs.unfold.js
-//= require hs.validation.js
-//= require jquery.fancybox.min.js
-//= require jquery.mCustomScrollbar.concat.min.js
-//= require jquery-migrate.min.js
+//= require custom.js
+//
 //= require jquery.min.js
-//= require jquery.validate.min.js
+//= require jquery-migrate.min.js
 //= require popper.min.js
-//= require slick.js
-//= require svg-injector.map.js
+//= require bootstrap.min.js
+//= require hs.megamenu.js
+//= require svg-injector.min.js
+//= require jquery.mCustomScrollbar.concat.min.js
+//= require jquery.validate.min.js
+//= require jquery.fancybox.min.js
 //= require typed.min.js
+//= require slick.js
+//= require hs.core.js
+//= require hs.header.js
+//= require hs.unfold.js
+//= require hs.focus-state.js
+//= require hs.malihu-scrollbar.js
+//= require hs.validation.js
+//= require hs.fancybox.js
+//= require hs.slick-carousel.js
+//= require hs.show-animation.js
+//= require hs.svg-injector.js
+//= require hs.go-to.js
+//
+// Bootstrap js is called from application.html.erb, end of the file.
