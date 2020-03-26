2c118aed18ffe6349f9af56307195dd50f0645bc
Raphael Chappert <raphael.chappert@hotmail.fr>
2020-03-26 18:47:27 +0100

    edit controller company profile

--- app/controllers/company_profiles_controller.rb
+++ app/controllers/company_profiles_controller.rb
@@ -10,7 +10,6 @@ class CompanyProfilesController < ApplicationController
   def update
     @company_profile = CompanyProfile.find(params[:id])
     if @company_profile.update(company_profile_params)
-
       redirect_to company_profile_path(@company_profile), notice: "Votre profil a bien été enregistré."
     else
       render :edit, alert: "Veuillez rentrer des champs valides."
