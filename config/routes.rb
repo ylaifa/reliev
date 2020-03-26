356feb4721942b6089421add5e678119dc338365
Raphael Chappert <raphael.chappert@hotmail.fr>
2020-03-26 18:48:42 +0100

    edit routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,14 +3,19 @@ Rails.application.routes.draw do
   devise_for :masseurs,  controllers: { registrations: "registrations" }
   devise_for :companies, controllers: { registrations: "registrations" }
 
+  resources :companies do
+    resources :employees
+  end
+
   resources  :masseur_profiles,  only: [:index, :show, :edit, :update]
   resources  :company_profiles,  only: [:show, :edit, :update]
   resources  :employee_profiles, only: [:show, :edit, :update]
 
   resources  :search, only: [:index]
-  
-  root 'welcome#index'
+
   get 'about',   to: 'static_pages#about'
   get 'contact', to: 'static_pages#contact'
+
+  root 'welcome#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
