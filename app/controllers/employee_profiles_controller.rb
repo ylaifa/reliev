c855dc992ddea8cc77057e8ffad925ee0d4bc1fe
Yousri <laifayousri@gmail.com>
2020-03-27 10:59:17 +0100

    edit employee_profiles controller

--- app/controllers/employee_profiles_controller.rb
+++ app/controllers/employee_profiles_controller.rb
@@ -3,24 +3,15 @@ class EmployeeProfilesController < ApplicationController
   before_action :check_privileges!, only: [:show, :edit]
 
   def show
-    @employee_profile = EmployeeProfile.find_by(employee_id: params[:id])
-    if @employee_profile == nil
-      @employee_profile = EmployeeProfile.find(params[:id])
-    end
+    @employee_profile = EmployeeProfile.find(params[:id])
   end
 
   def edit
-    @employee_profile = EmployeeProfile.find_by(employee_id: params[:id])
-    if @employee_profile == nil
-      @employee_profile = EmployeeProfile.find(params[:id])
-    end
+    @employee_profile = EmployeeProfile.find(params[:id])
   end
 
   def update
-    @employee_profile = EmployeeProfile.find_by(employee_id: params[:id])
-    if @employee_profile == nil
-      @employee_profile = EmployeeProfile.find(params[:id])
-    end
+    @employee_profile = EmployeeProfile.find(params[:id])
     if @employee_profile.update(employee_profile_params)
       redirect_to employee_profile_path(@employee_profile), notice: "Votre profil a bien été enregistré."
     else
