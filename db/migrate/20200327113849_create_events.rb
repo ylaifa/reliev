e951017762f763513c0fb25d274f7fb0ae7b028b
Yousri <laifayousri@gmail.com>
2020-03-27 12:42:36 +0100

    edit migration

--- db/migrate/20200327113849_create_events.rb
+++ db/migrate/20200327113849_create_events.rb
@@ -4,9 +4,10 @@ class CreateEvents < ActiveRecord::Migration[5.2]
       t.string :title
       t.text :description
       t.string :masseur
-      t.string :participating_employees
+      t.string :participating_employees, array: true, default: []
       t.string :moment
       t.interger :maximum_number_of_participation
+      t.belongs_to :company, index: true
 
       t.timestamps
     end
