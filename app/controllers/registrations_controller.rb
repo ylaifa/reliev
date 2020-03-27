da79fadfc284feada8b73d3335821ba657a65e90
Yousri <laifayousri@gmail.com>
2020-03-27 10:59:48 +0100

    edit registrations_controller

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -2,11 +2,9 @@ class RegistrationsController < Devise::RegistrationsController
   private
 
   def after_sign_up_path_for(resource)
-    if @employee
-      edit_employee_profile_path(@employee)
-    elsif @masseur
+    if @masseur
       edit_masseur_profile_path(@masseur.masseur_profile)
-    elsif @company
+    else
       edit_company_profile_path(@company.company_profile)
     end
   end
