076faf57fbe9e1886cb6802120d50d1730f937d9
Yousri <laifayousri@gmail.com>
2020-03-27 13:12:21 +0100

    run migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_27_114145) do
+ActiveRecord::Schema.define(version: 2020_03_27_120947) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -46,6 +46,8 @@ ActiveRecord::Schema.define(version: 2020_03_27_114145) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "city"
+    t.string "phone_number"
+    t.string "business_line"
     t.index ["company_id"], name: "index_company_profiles_on_company_id"
   end
 
