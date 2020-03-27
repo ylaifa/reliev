509e9398a3dc79e4313ebb3adea0a4db1b322281
Raphael Chappert <raphael.chappert@hotmail.fr>
2020-03-27 01:41:12 +0100

    edit routes with send_invitation patch

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,15 @@
 Rails.application.routes.draw do
   devise_for :employees, controllers: { registrations: "registrations" }
   devise_for :masseurs,  controllers: { registrations: "registrations" }
-  devise_for :companies, controllers: { registrations: "registrations" }
-
-  devise_scope :company do 
-    match "/companies/:project_id/invitations/new", :to => "invitations#new", :via => "get", :as => "new_project_invitation"
-  end
+  devise_for :companies, controllers: { registrations: "registrations" } 
   
   resources  :masseur_profiles,  only: [:index, :show, :edit, :update]
   resources  :company_profiles,  only: [:show, :edit, :update]
   resources  :employee_profiles, only: [:show, :edit, :update]
 
   resources  :search, only: [:index]
+
+  patch 'send_invite_to_employee', to: 'company_profiles#send_invite_to_employee'
 
   get 'about',   to: 'static_pages#about'
   get 'contact', to: 'static_pages#contact'
