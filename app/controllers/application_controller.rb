a73e808d8e05d194e1cfbd0a8e6be77703997811
Raphael Chappert <raphael.chappert@hotmail.fr>
2020-03-27 02:16:29 +0100

    after sign in application_controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,13 @@
 class ApplicationController < ActionController::Base
+
+  def after_sign_in_path_for(resource)
+    if @employee
+      employee_profile_path(@employee)
+    elsif @masseur
+      masseur_profile_path(@masseur.masseur_profile)
+    else
+      company_profile_path(@company.company_profile)
+    end
+  end
+  
 end
