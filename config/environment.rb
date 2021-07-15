a4540a0ba6c4f02a38a4ff218ead9cc2678442c8
Yousri LAÏFA <laifayousri@gmail.com>
2021-07-15 23:01:41 +0200

    edit environment.rb change smtp settings

--- config/environment.rb
+++ config/environment.rb
@@ -4,12 +4,11 @@ require_relative 'application'
 # Initialize the Rails application.
 Rails.application.initialize!
 
-ActionMailer::Base.smtp_settings = {
-  :user_name => ENV['SENDGRID_LOGIN'],
-  :password => ENV['SENDGRID_PWD'],
-  :domain => 'https://reliev-prod.herokuapp.com/',
-  :address => 'smtp.sendgrid.net',
-  :port => 587,
-  :authentication => :plain,
-  :enable_starttls_auto => true
+ActionMailer::Base.smtp_settings =   {
+  :address            => 'smtp.gmail.com',
+  :port               => 587,
+  :domain             => 'gmail.com',
+  :authentication     => :plain,
+  :user_name          => ENV['GMAIL_LOGIN'],
+  :password           => ENV['GMAIL_PWD']
 }
