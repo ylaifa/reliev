6a3e4cec7831bd9431529ce1fbab2f67344ac9e5
Yousri LAÏFA <laifayousri@gmail.com>
2023-04-25 00:26:55 +0200

    edit config/application.rb

--- config/application.rb
+++ config/application.rb
@@ -15,5 +15,6 @@ module Reliev
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
+    config.assets.js_compressor = false
   end
 end
