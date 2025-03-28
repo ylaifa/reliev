cd4aabd805688db5c74ef2d61371aab34435eae6
ylaifa <laifayousri@gmail.com>
2025-03-28 18:04:34 +0100

    Fix: config for db seed

--- config/application.rb
+++ config/application.rb
@@ -9,7 +9,10 @@ Bundler.require(*Rails.groups)
 module Reliev
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 5.2
+    config.load_defaults 7.1
+
+    # Configure the cache serialization format for Rails 7.1
+    config.active_support.cache_format_version = 7.0
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
